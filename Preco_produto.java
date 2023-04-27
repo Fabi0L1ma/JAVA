a49c2151aa0e45a446d5882c73d2567f96935532
Fábio Lima <119767213+Fabi0L1ma@users.noreply.github.com>
2023-04-27 18:23:06 -0300

    Add files via upload

---
+++ Preco_produto.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Medias_Alturas {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        float preco = 0, remover_ADD_Preco = 0,  valorEmEstoque = 0;
+        int add_rem = 0, qtdEstoque;
+
+        System.out.print("Digite o nome do produto: ");
+        nome = sc.next();
+        System.out.print("Preco do produto: ");
+        preco = sc.nextFloat();
+        System.out.print("QTD do produto: ");
+        qtdEstoque = sc.nextInt();
+
+        valorEmEstoque = preco * qtdEstoque;
+
+        System.out.println("Dados do produto: " + nome + ", R$" + preco + ", " + qtdEstoque + " unidades, " + "Total: R$" + valorEmEstoque);
+
+        System.out.print("Informe numero de produtos a serem adicionados no estoque: ");
+        add_rem = sc.nextInt();
+
+        qtdEstoque += add_rem;
+        remover_ADD_Preco = add_rem * preco;
+        valorEmEstoque = valorEmEstoque + remover_ADD_Preco;
+
+        System.out.println("Dados atualizados: " + nome + ", R$" + preco + ", " + qtdEstoque + ", " + "Total: " + valorEmEstoque);
+
+        System.out.print("Informe o numero de produtos a serem adicionados no estoque: ");
+        add_rem = sc.nextInt();
+
+        qtdEstoque -= add_rem;
+        remover_ADD_Preco = add_rem * preco;
+        valorEmEstoque = valorEmEstoque - remover_ADD_Preco;
+
+        System.out.println("Dados atualizados: " + nome + ", R$" + preco + ", " + qtdEstoque + ", " + "Total: " + valorEmEstoque);
+
+    }
+}
+
+
