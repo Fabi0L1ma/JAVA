420fc8d5ba6ddd87233c676ada98acb66baec61c
Fábio Lima <119767213+Fabi0L1ma@users.noreply.github.com>
2023-04-27 17:46:09 -0300

    Add files via upload

---
+++ Medias_Alturas.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String nome[] = new String[5];
+        float altura[] = new float[5];
+        char sexo[] = new char[5];
+        float maiorAltura = 0, menorAltura, AlturaH = 0, medAlturaH = 0;
+        int i, qtd_H = 0, numMulheres = 0;
+
+        for(i = 0; i < 5; i++){
+            System.out.print("Nome: ");
+            nome[i] = sc.next();
+            System.out.print("Altura: ");
+            altura[i] = sc.nextFloat();
+            System.out.print("Sexo [M] ou [F]: ");
+            sexo[i] = sc.next().charAt(0);
+        }
+
+        menorAltura = altura[0];
+
+        for (i = 0; i < 5; i++){
+            if (altura[i] > maiorAltura){
+                maiorAltura = altura[i];
+            }
+            if (altura[i] < menorAltura){
+                menorAltura = altura[i];
+            }
+            if (sexo[i] =='M'){
+                AlturaH += altura[i];
+                qtd_H += 1;
+            }else {
+                numMulheres += 1;
+            }
+        }
+
+        medAlturaH = AlturaH/qtd_H;
+
+        System.out.println("Maior altura: " + maiorAltura);
+        System.out.println("Menor altura: " + menorAltura);
+        System.out.println("Media de altura de homens: " + medAlturaH);
+        System.out.println("Numeros de mulheres: " + numMulheres);
+
+    }
+}
+
+
